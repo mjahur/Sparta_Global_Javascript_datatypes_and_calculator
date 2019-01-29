3e79a2c8bae8017c063a998746733231d5ced123
Mishu <mjahur@spartaglobal.com>
2019-01-29 01:39:52 +0000

    Completed homework

---
+++ simple_calculator/index.js
@@ -0,0 +1,5 @@
+var first_number = prompt ("Enter first number", "number");
+var operation = prompt ("Enter operation", "+, -, * or /");
+var second_number = prompt ("Enter second_number", "number");
+alert(eval(first_number + operation + second_number));
+alert("Refresh for new calculation")
---
+++ starter-code-datatypes/starter-code/lab.js
@@ -0,0 +1,31 @@
+// create a variable called first_name that contains a string
+var first_name = "Mishu";
+// create an array that contains three names
+var names_array = ["Mishu", "Mousumi", "Jahur"];
+// use your first_name variable for the third name
+names_array[2] = first_name;
+// create a variable called number_of_names that contains the number of people in your names array
+var number_of_names = names_array.length;
+// create a variable called age and put an appropriate data type in it
+var age = 33;
+// create a variable called younger that contains a third of this age
+var younger = age / 3;
+// create a null variable called empty
+var empty = null;
+// create a variable called phone_number and store this number 07888777396
+// ( think about what type of data would be best for this )
+var phone_number = "07888777396";
+// create a variable called friend
+// extract the second value from your names_array and put it in friend
+var friend = names_array[1];
+// create a variable called quote that contains the following sentence
+// I'm not a number, I'm fairly sure I'm a String
+var sentence = "I'm not a number, I'm fairly sure I'm a String";
+// create a variable called sentence_length that contains the length of the sentence
+var sentence_length = sentence.length;
+// BONUS
+// Research how to create a JSON object called person
+person = {
+  "first_name":"Mishu"
+}
+// Use the person object tests to add the correct data
